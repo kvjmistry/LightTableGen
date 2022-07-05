c44af61033c01682f2115fd68bdbad28ac5be0da
Krishan Mistry <kvjmistry@gmail.com>
2022-07-05 12:58:58 -0500

    Update launch job script

--- job/launch_jobs.sh
+++ job/launch_jobs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # JOB=NEW_PSF
-JOB=NEW_S1_LT
+# JOB=NEW_S1_LT
+JOB=NEW_S2_LT
 
 mkdir $JOB
 cd $JOB
 cp ../${JOB}_job.sh .
 
-sbatch --array=1-500 ${JOB}_job.sh
+sbatch --array=1-5000 ${JOB}_job.sh
